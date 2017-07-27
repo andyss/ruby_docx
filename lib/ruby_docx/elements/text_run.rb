6c335e4a31eb8f960e8735faa64606f6304d1b0c
Joey Lin <joeyoooooo@gmail.com>
2017-07-27 23:11:51 +0800

    remove old files

--- lib/ruby_docx/elements/text_run.rb
+++ lib/ruby_docx/elements/text_run.rb
@@ -3,12 +3,43 @@ module RubyDocx::Elements
     attr_reader :font_name, :font_size, :color
     attr_accessor :content
 
+    def underline?
+      @node.xpath(".//w:u").size > 0
+    end
+
     def to_s
-      self.elements.map(&:to_s).join(" ")
+      if underline?
+        self.elements.map do |ele|
+          if ele.class == RubyDocx::Elements::Text
+            ele.to_s.gsub(/ /, "_")
+          else
+            ele.to_s
+          end
+        end.join(" ")
+
+      else
+        self.elements.map(&:to_s).join(" ")
+      end
     end
 
     def to_html
-      "<span>#{self.elements.map(&:to_html).join}</span>"
+      if underline?
+        s = "<span>"
+
+        self.elements.map do |ele|
+          if ele.class == RubyDocx::Elements::Text
+            s += ele.to_s.gsub(/ /, "_")
+          else
+            s += ele.to_html
+          end
+        end
+
+        s += "</span>"
+
+        s
+      else
+        "<span>#{self.elements.map(&:to_html).join}</span>"
+      end
     end
 
   end
